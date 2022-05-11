9ffd9c9fb12612ddfac89be822d1494f30030051
Ahmed Shalaby <ashalaby@tahmeellogistics.com>
2022-05-11 12:17:17 +0200

    add sidebar 2

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+function loadLinks() {
+  links = [
+    {
+      text: 'page 1',
+      href: 'index1.html',
+    },
+
+    // add more links
+  ]
+  const sidebar = document.getElementById('sidebar2')
+
+  // put these steps in a loop
+  let link = document.createElement('a')
+  link.innerHTML = links[0].text
+  link.setAttribute('href', links[0].text)
+  link.classList.add('nav-link')
+  sidebar.appendChild(link)
+}
 // let links = document.querySelector('sidebar');
 let url = window.location.href
 let fir = document.getElementById('fir').href
